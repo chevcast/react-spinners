e1961021d69e7e767013aa80262dcad0de2d7934
Alex Ford <alex.ford@codetunnel.com>
2017-12-01 11:27:57 -0700

    Changed build to dist.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 const PATHS = {
   src: path.join(__dirname, './src'),
-  build: path.join(__dirname, './build')
+  build: path.join(__dirname, './dist')
 }
 
 module.exports = {
